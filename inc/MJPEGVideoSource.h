462b2ef80aaa9a3b67e2d99bb2dbbaafaf134c79
Michel Promonet <michel.promonet@free.fr>
2017-06-03 16:19:55 +0200

    add debug log for JPEG

--- inc/MJPEGVideoSource.h
+++ inc/MJPEGVideoSource.h
@@ -35,62 +35,76 @@ class MJPEGVideoSource : public JPEGVideoSource
       {
          MJPEGVideoSource* source = (MJPEGVideoSource*)clientData;
          source->afterGettingFrame(frameSize, numTruncatedBytes, presentationTime, durationInMicroseconds);
-      }        
+      } 
+      
       void afterGettingFrame(unsigned frameSize,unsigned numTruncatedBytes,struct timeval presentationTime,unsigned durationInMicroseconds)
       {
-         int headerSize = 0;
-         bool headerOk = false;
-         fFrameSize = 0;
+        int headerSize = 0;
+        fFrameSize = 0;
                   
-         for (unsigned int i = 0; i < frameSize ; ++i)
-         {
+        unsigned int i = 0; 
+        while ( (i<frameSize) && (headerSize==0) ) {
             // SOF
-            if ( (i+11) < frameSize  && fTo[i] == 0xFF && fTo[i+1] == 0xC0 )
-            {
-               m_height = (fTo[i+5]<<5)|(fTo[i+6]>>3);
-               m_width  = (fTo[i+7]<<5)|(fTo[i+8]>>3);
-               m_type   = (fTo[i+11] - 0x21);
-               LOG(INFO) << "width:" << (int)m_width << " height:" << (int)m_height << " type:"<< (int)m_type;
+            if ( ((i+11) < frameSize)  && (fTo[i] == 0xFF) && (fTo[i+1] == 0xC0) ) {
+                int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+                LOG(DEBUG) << "SOF length:" << length;
 
+                m_height = (fTo[i+5]<<5)|(fTo[i+6]>>3);
+                m_width  = (fTo[i+7]<<5)|(fTo[i+8]>>3);
+
+                int hv_subsampling = fTo[i+11];
+                if (hv_subsampling == 0x21 ) {
+                    m_type = 0; // JPEG 4:2:2
+                } else if (hv_subsampling == 0x22 ) {
+                    m_type = 1; // JPEG 4:2:0
+                } else {
+                    LOG(NOTICE) << "not managed sampling:0x" << std::hex << hv_subsampling;
+                    m_type = 255;
+                }
+                LOG(INFO) << "width:" << (int)(m_width<<3) << " height:" << (int)(m_height<<3) << " type:"<< (int)m_type;
+
+                i+=length;
             }
             // DQT
-            if ( (i+5+64) < frameSize && (fTo[i] == 0xFF) && (fTo[i+1] == 0xDB))
-            {
-               unsigned int quantSize = fTo[i+3]-4;
-               unsigned int quantIdx = fTo[i+4];
-               if (quantSize*quantIdx+quantSize <= sizeof(m_qTable))
-               {
-                  memcpy(m_qTable + quantSize*quantIdx, fTo + i + 5, quantSize);
-                  if (quantSize*quantIdx+quantSize > m_qTableSize)
-                  {
-                     m_qTableSize = quantSize*quantIdx+quantSize;
-                     LOG(NOTICE) << "Quantization table idx:" << quantIdx << " size:" << quantSize << " total size:" << m_qTableSize;
-                  }
-               }
-            }
-            // End of header
-            if ( (i+1) < frameSize && fTo[i] == 0x3F && fTo[i+1] == 0x00 )
-            {
-               headerOk = true;
-               headerSize = i+2;
-               break;
-            }
-         }
+            else if ( ( (i+5+64) < frameSize)  && (fTo[i] == 0xFF) && (fTo[i+1] == 0xDB)) {
+                int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+                LOG(DEBUG) << "DQT length:" << length;
 
-         if (headerOk)
-         {
+                unsigned int quantSize = length-4;
+                unsigned int quantIdx = fTo[i+4];
+                if (quantSize*quantIdx+quantSize <= sizeof(m_qTable)) {
+                    memcpy(m_qTable + quantSize*quantIdx, fTo + i + 5, quantSize);
+                    if (quantSize*quantIdx+quantSize > m_qTableSize) {
+                        m_qTableSize = quantSize*quantIdx+quantSize;
+                        LOG(NOTICE) << "Quantization table idx:" << quantIdx << " size:" << quantSize << " total size:" << m_qTableSize;
+                    }
+                }
+
+                i+=length;	       
+            }
+            // SOS
+            else if ( ((i+1) < frameSize) && (fTo[i] == 0xFF) && (fTo[i+1] == 0xDA) ) {            
+                int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+                LOG(DEBUG) << "SOS length:" << length;                
+                
+                headerSize = i+length+2;                
+            } else {
+                i++;
+            }
+        }
+
+        if (headerSize != 0) {
+            LOG(DEBUG) << "headerSize:" << headerSize;
             fFrameSize = frameSize - headerSize;
             memmove( fTo, fTo + headerSize, fFrameSize );
-         }
-         else
-         {
+        } else {
             LOG(NOTICE) << "Bad header => dropping frame";
-         }
+        }
 
-         fNumTruncatedBytes = numTruncatedBytes;
-         fPresentationTime = presentationTime;
-         fDurationInMicroseconds = durationInMicroseconds;
-         afterGetting(this);
+        fNumTruncatedBytes = numTruncatedBytes;
+        fPresentationTime = presentationTime;
+        fDurationInMicroseconds = durationInMicroseconds;
+        afterGetting(this);
       }
       virtual u_int8_t type() { return m_type; };
       virtual u_int8_t qFactor() { return 128; };
