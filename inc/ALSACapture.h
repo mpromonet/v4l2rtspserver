8fd18e3c23d2255cceaaff39a9ef45b434af6722
Michel Promonet <michel.promonet@free.fr>
2017-07-30 19:37:39 +0200

    extract code from include file

--- inc/ALSACapture.h
+++ inc/ALSACapture.h
@@ -3,7 +3,7 @@
 ** support, and with no warranty, express or implied, as to its usefulness for
 ** any purpose.
 **
-** ALSACapture.cpp
+** ALSACapture.h
 ** 
 ** V4L2 RTSP streamer                                                                 
 **                                                                                    
@@ -32,145 +32,16 @@ struct ALSACaptureParameters
 class ALSACapture 
 {
 	public:
-		static ALSACapture* createNew(const ALSACaptureParameters & params) 
-		{ 
-			ALSACapture* capture = new ALSACapture(params);
-			if (capture) 
-			{
-				if (capture->getFd() == -1) 
-				{
-					delete capture;
-					capture = NULL;
-				}
-			}
-			return capture; 
-		}
-		virtual ~ALSACapture()
-		{
-			this->close();
-		}
-		void close()
-		{
-			if (m_pcm != NULL)
-			{
-				snd_pcm_close (m_pcm);
-				m_pcm = NULL;
-			}
-		}
+		static ALSACapture* createNew(const ALSACaptureParameters & params) ;
+		virtual ~ALSACapture();
+		void close();
 	
 	protected:
-		ALSACapture(const ALSACaptureParameters & params) : m_pcm(NULL), m_bufferSize(0), m_periodSize(0), m_params(params)
-		{
-			LOG(NOTICE) << "Open ALSA device: \"" << params.m_devName << "\"";
-			
-			snd_pcm_hw_params_t *hw_params = NULL;
-			int err = 0;
-			
-			// open PCM device
-			if ((err = snd_pcm_open (&m_pcm, m_params.m_devName.c_str(), SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-				LOG(ERROR) << "cannot open audio device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-			}
-						
-			// configure hw_params
-			else if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-				LOG(ERROR) << "cannot allocate hardware parameter structure device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			else if ((err = snd_pcm_hw_params_any (m_pcm, hw_params)) < 0) {
-				LOG(ERROR) << "cannot initialize hardware parameter structure device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}			
-			else if ((err = snd_pcm_hw_params_set_access (m_pcm, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-				LOG(ERROR) << "cannot set access type device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			else if ((err = snd_pcm_hw_params_set_format (m_pcm, hw_params, m_params.m_fmt)) < 0) {
-				LOG(ERROR) << "cannot set sample format device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			else if ((err = snd_pcm_hw_params_set_rate_near (m_pcm, hw_params, &m_params.m_sampleRate, 0)) < 0) {
-				LOG(ERROR) << "cannot set sample rate device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			else if ((err = snd_pcm_hw_params_set_channels (m_pcm, hw_params, m_params.m_channels)) < 0) {
-				LOG(ERROR) << "cannot set channel count device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			else if ((err = snd_pcm_hw_params (m_pcm, hw_params)) < 0) {
-				LOG(ERROR) << "cannot set parameters device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			
-			// get buffer size
-			else if ((err = snd_pcm_get_params(m_pcm, &m_bufferSize, &m_periodSize)) < 0) {
-				LOG(ERROR) << "cannot get parameters device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}
-			
-			// start capture
-			else if ((err = snd_pcm_prepare (m_pcm)) < 0) {
-				LOG(ERROR) << "cannot prepare audio interface for use device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}			
-			else if ((err = snd_pcm_start (m_pcm)) < 0) {
-				LOG(ERROR) << "cannot start audio interface for use device: " << m_params.m_devName << " error:" <<  snd_strerror (err);
-				this->close();
-			}			
-			
-			LOG(NOTICE) << "ALSA device: \"" << m_params.m_devName << "\" buffer_size:" << m_bufferSize << " period_size:" << m_periodSize << " rate:" << m_params.m_sampleRate;
-		}
+		ALSACapture(const ALSACaptureParameters & params);
 			
 	public:
-		virtual size_t read(char* buffer, size_t bufferSize)
-		{
-			size_t size = 0;
-			int fmt_phys_width_bytes = 0;
-			if (m_pcm != 0)
-			{
-				int fmt_phys_width_bits = snd_pcm_format_physical_width(m_params.m_fmt);
-				fmt_phys_width_bytes = fmt_phys_width_bits / 8;
-
-				snd_pcm_sframes_t ret = snd_pcm_readi (m_pcm, buffer, m_periodSize*fmt_phys_width_bytes);
-				LOG(DEBUG) << "ALSA buffer in_size:" << m_periodSize*fmt_phys_width_bytes << " read_size:" << ret;
-				if (ret > 0) {
-					size = ret;				
-					
-					// swap if capture in not in network order
-					if (!snd_pcm_format_big_endian(m_params.m_fmt)) {
-						for(unsigned int i = 0; i < size; i++){
-							char * ptr = &buffer[i * fmt_phys_width_bytes * m_params.m_channels];
-							
-							for(unsigned int j = 0; j < m_params.m_channels; j++){
-								ptr += j * fmt_phys_width_bytes;
-								for (int k = 0; k < fmt_phys_width_bytes/2; k++) {
-									char byte = ptr[k];
-									ptr[k] = ptr[fmt_phys_width_bytes - 1 - k];
-									ptr[fmt_phys_width_bytes - 1 - k] = byte; 
-								}
-							}
-						}
-					}
-				}
-			}
-			return size * m_params.m_channels * fmt_phys_width_bytes;
-		}
-		
-		virtual int getFd()
-		{
-			unsigned int nbfs = 1;
-			struct pollfd pfds[nbfs]; 
-			pfds[0].fd = -1;
-			
-			if (m_pcm != 0)
-			{
-				int count = snd_pcm_poll_descriptors_count (m_pcm);
-				int err = snd_pcm_poll_descriptors(m_pcm, pfds, count);
-				if (err < 0) {
-					fprintf (stderr, "cannot snd_pcm_poll_descriptors (%s)\n", snd_strerror (err));
-				}
-			}
-			return pfds[0].fd;
-		}
+		virtual size_t read(char* buffer, size_t bufferSize);		
+		virtual int getFd();
 		
 		virtual unsigned long getBufferSize() { return m_bufferSize; };
 		virtual int getWidth()  {return -1;}
---
+++ src/MJPEGVideoSource.cpp
@@ -0,0 +1,98 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** MJPEGVideoSource.cpp
+** 
+** V4L2 RTSP streamer                                                                 
+**                                       
+** MJPEG Source for RTSP server 
+**                                                                                    
+** -------------------------------------------------------------------------*/
+
+#include "MJPEGVideoSource.h"
+
+      
+void MJPEGVideoSource::afterGettingFrame(unsigned frameSize,unsigned numTruncatedBytes,struct timeval presentationTime,unsigned durationInMicroseconds)
+{
+	int headerSize = 0;
+	fFrameSize = 0;
+		  
+	unsigned int i = 0; 
+	while ( (i<frameSize) && (headerSize==0) ) {
+	    // SOF
+	    if ( ((i+11) < frameSize)  && (fTo[i] == 0xFF) && (fTo[i+1] == 0xC0) ) {
+		int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+		LOG(DEBUG) << "SOF length:" << length;
+
+		m_height = (fTo[i+5]<<5)|(fTo[i+6]>>3);
+		m_width  = (fTo[i+7]<<5)|(fTo[i+8]>>3);
+
+		int hv_subsampling = fTo[i+11];
+		if (hv_subsampling == 0x21 ) {
+		    m_type = 0; // JPEG 4:2:2
+		} else if (hv_subsampling == 0x22 ) {
+		    m_type = 1; // JPEG 4:2:0
+		} else {
+		    LOG(NOTICE) << "not managed sampling:0x" << std::hex << hv_subsampling;
+		    m_type = 255;
+		}
+		LOG(INFO) << "width:" << (int)(m_width<<3) << " height:" << (int)(m_height<<3) << " type:"<< (int)m_type;
+
+		i+=length+2;
+	    }
+	    // DQT
+	    else if ( ( (i+5+64) < frameSize)  && (fTo[i] == 0xFF) && (fTo[i+1] == 0xDB)) {
+		int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+		LOG(DEBUG) << "DQT length:" << length;
+
+		unsigned int precision = fTo[i+4]<<4;
+		unsigned int quantIdx  = fTo[i+4]&0x0f;
+		unsigned int quantSize = length-3;
+		if (quantSize*quantIdx+quantSize <= sizeof(m_qTable)) {
+		    memcpy(m_qTable + quantSize*quantIdx, fTo + i + 5, quantSize);
+		    if (quantSize*quantIdx+quantSize > m_qTableSize) {
+			m_qTableSize = quantSize*quantIdx+quantSize;
+			LOG(NOTICE) << "Quantization table idx:" << quantIdx << " precision:" << precision << " size:" << quantSize << " total size:" << m_qTableSize;
+		    }
+		}
+
+		i+=length+2;	       
+	    }
+	    // SOS
+	    else if ( ((i+1) < frameSize) && (fTo[i] == 0xFF) && (fTo[i+1] == 0xDA) ) {            
+		int length = (fTo[i+2]<<8)|(fTo[i+3]);		    
+		LOG(DEBUG) << "SOS length:" << length;                
+		
+		headerSize = i+length+2;                
+	    } else {
+		i++;
+	    }
+	}
+
+	if (headerSize != 0) {
+	    LOG(DEBUG) << "headerSize:" << headerSize;
+	    fFrameSize = frameSize - headerSize;
+	    memmove( fTo, fTo + headerSize, fFrameSize );
+	} else {
+	    LOG(NOTICE) << "Bad header => dropping frame";
+	}
+
+	fNumTruncatedBytes = numTruncatedBytes;
+	fPresentationTime = presentationTime;
+	fDurationInMicroseconds = durationInMicroseconds;
+	afterGetting(this);
+}
+
+u_int8_t const* MJPEGVideoSource::quantizationTables( u_int8_t& precision, u_int16_t& length )
+{
+	length = 0;
+	precision = 0;
+	if (m_qTableSize > 0)
+	{
+		length = m_qTableSize;
+		precision = m_precision;
+	}
+	return m_qTable;            
+}
