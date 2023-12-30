3320b9173a338c869cd62ddad8d6ebd6982b8662
Michel Promonet <michel.promonet@free.fr>
2023-12-30 21:25:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -250,6 +250,7 @@ int main(int argc, char** argv)
 	
 	// default format tries
 	if ((videoformatList.empty()) && (format!=0)) {
+		videoformatList.push_back(V4L2_PIX_FMT_H265);
 		videoformatList.push_back(V4L2_PIX_FMT_H264);
 		videoformatList.push_back(V4L2_PIX_FMT_MJPEG);
 		videoformatList.push_back(V4L2_PIX_FMT_JPEG);
